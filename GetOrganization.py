a55e8fff8938b24b2e828b5a5fdca0e0f140db6d
UGD <brunoenee@gmail.com>
2022-04-11 17:17:49 +0300

    Retour data

--- GetOrganization.py
+++ GetOrganization.py
@@ -1,5 +1,14 @@
-import boto3
-region = 'us-east-1'
-session = boto3.Session(profile_name="WORKMAIL", region_name=region)
-client = session.client('workmail')
-client.list_organizations()['OrganizationSummaries']
+def GetOrganizationInfo(p="Id"):
+    import boto3
+    region = 'us-east-1'
+    data = []
+    session = boto3.Session(profile_name="WORKMAIL", region_name=region)
+    client = session.client('workmail')
+    for info in client.list_organizations()['OrganizationSummaries']:
+        if p == "Id":
+            data.append(info['OrganizationId'])
+        if p == "Domain":
+            data.append(info['DefaultMailDomain'])
+    return data
+
+GetOrganizationInfo(p="Domain")
