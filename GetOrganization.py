a3847441ff54cd8813894cd582f2e74d74640418
UGD <brunoenee@gmail.com>
2022-04-11 17:08:17 +0300

    Region parametre

--- GetOrganization.py
+++ GetOrganization.py
@@ -1,5 +1,5 @@
 import boto3
-
-session = boto3.Session(profile_name="WORKMAIL", region_name=regions[0])
+region = 'us-east-1'
+session = boto3.Session(profile_name="WORKMAIL", region_name=region)
 client = session.client('workmail')
 client.list_organizations()['OrganizationSummaries']
